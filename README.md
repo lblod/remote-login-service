292978dd343ddc76a0961cd432535d848a8afcf3
Niels Vandekeybus <progster@gmail.com>
2019-03-06 16:23:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# remote login service
+this service is responsible for retrieving user information and linking it to a user session.
+
+## configuration
+Example docker-compose snippet (for usage with seas):
+```
+services:
+  remotelogin:
+    image: lblod/remotelogin
+    environment:
+      MU_APPLICATION_GRAPH: 'http://mu.semte.ch/graphs/public'
+    volumes:
+      - './config/remotelogin:/app/config'
+```
+
+An example config file is available in the config folder of this repository.
+
+## usage
+### request
+```
+    POST /remote-login HTTP/1.1
+    Content-Type: application/vnd.api+json
+    Accept: application/vnd.api+json
+
+    {
+      "data": {
+          "type": "remote-login",
+          "attributes": {
+              "client-id": "1c5d34ca-4a76-456e-9cac-5c39fabeb90f",
+              "user-token": "60f4b6be-bf44-442d-ad17-c0006e463f15"
+          }
+      }
+    }
+```
+### response
+
+#### 201 CREATED
+```
+    HTTP/1.1 201 Created
+    Content-Type: application/vnd.api+json
+    {
+      "data": {
+        "type": "remote-login",
+        "id": "1c5d34ca-4a76-456e-9cac-5c39fabeb90f",
+        "relationships": {
+          "group": {
+            "links": {
+              "related": "/bestuurseenheden/${groupId}"
+            },
+            "data": {
+              "type": "bestuurseenheden",
+              "id": "${bestuurseenheidID}"
+            }
+          },
+          "accounts": {
+            "links": {
+              "related": "/accounts/${accountI}"
+            },
+            "data": {
+              "type": "accounts",
+              "id": "${accountId}"
+            }
+          },
+          "session": {
+            "links": {
+              "related": "/sessions/current"
+            },
+            "id": "${sessionID}"
+          }
+        }
+      }
+    }
+```
+#### 404 NOT FOUND
+
+If a 404 was returned by the identity server (because no user could be
+found for the specified user token), the remote login will also return a
+404.
+```
+    HTTP/1.1 404 Not Found
+    Content-Type: application/vnd.api+j
+    { errors: ['error message'] }
+```
+#### 400 BAD REQUEST
+
+If the request could not be processed correctly a 400 is returned
+```
+    HTTP/1.1 400 Bad Request
+    Content-Type: application/vnd.api+json
+    { errors: ['error message'] }
+```
+#### 502 BAD GATEWAY
+
+If the identity endpoint does not respond or does not respond correctly
+a 502 bad gateway is returned
+```
+    HTTP/1.1 502 Bad Gateway
+    Content-Type: application/vnd.api+json
+    { errors: ['error message'] }
+```
+#### 500 SERVER ERROR
+
+In case of an unexpected error a 500 is returned
+```
+    HTTP/1.1 500 Server Error
+    Content-Type: application/vnd.api+json
+    { errors: ['error message'] }
+```
---
+++ app.js
@@ -0,0 +1,130 @@
+import { querySudo as query, updateSudo as update } from '@lblod/mu-auth-sudo';
+import { app, uuid, errorHandler } from 'mu';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import needle from 'needle';
+import { removeOldSessions, ensureUserAndAccount, insertNewSessionForAccount, selectBestuurseenheid } from './lib/session';
+
+function get(obj, path) {
+  const keys = path.split('.');
+  var o = obj;
+  for (const key of keys) {
+    if (o.hasOwnProperty(key))
+      o = o[key];
+    else {
+      return null;
+    }
+  }
+  return o;
+}
+function parseRequestBody(req) {
+  const errors = [];
+  if (! req.headers['mu-session-id'])
+    errors.push('session header is missing');
+  if (! req.headers['content-type'] || ! req.headers['content-type'].startsWith('application/vnd.api+json') )
+    errors.push('invalid content-type: ' + req.headers['content-type']);
+  var body = req.body;
+  if (! body.data || ! body.data.attributes || ! body.data.attributes["client-id"] || ! body.data.attributes["user-token"])
+    errors.push('invalid request body');
+  const clientId = body.data.attributes["client-id"];
+  const userToken = body.data.attributes["user-token"];
+  return {clientId,userToken, errors};
+}
+
+async function fetchIdentity(endpoint, userToken) {
+  const resp = await needle('get',`${endpoint}?user-token=${userToken}`);
+  return resp;
+}
+
+function getEndpointForClientId(clientId) {
+  const config = yaml.safeLoad(fs.readFileSync('/app/config/clients.yml', 'utf8'));
+  const client = config.clients.find((c) => c.id === clientId);
+  if (client)
+    return client.endpoint;
+  else
+    return null;
+}
+
+async function linkSessionToUser(body, sessionUri) {
+  await removeOldSessions(sessionUri);
+  const claims = {};
+  claims.id = get(body, 'data.id');
+  claims.given_name = get(body, 'data.attributes.given-name');
+  claims.family_name = get(body,'data.attributes.family-name');
+  claims.identifier = get(body, 'data.attributes.identifier');
+  const groupId = get(body, 'relationships.group.data.id');
+  const { accountUri, accountId } = await ensureUserAndAccount(claims, groupId);
+  const groupUri = await selectBestuurseenheid(groupId);
+  if (!groupUri)
+    throw "bestuurseenheid not found";
+  const { sessionId } = await insertNewSessionForAccount(accountUri, sessionUri, groupUri);
+  return { groupId, accountId, sessionId};
+}
+app.post('/remote-login', async function( req, res ) {
+  const {clientId, userToken, errors} = parseRequestBody(req);
+  if (errors.length > 0) {
+    res.status(400).send({errors: errors}).end();
+    return;
+  }
+
+  const endpoint = await getEndpointForClientId(clientId);
+  if (!endpoint) {
+    res.status(400).send({errors: ['invalid client-id']}).end();
+    return;
+  }
+  const identityResponse = await fetchIdentity(endpoint, userToken);
+  if (identityResponse.statusCode === 404) {
+    res.status(404).send({errors: ['error 404 returned by gateway']});
+  }
+  else if (identityResponse.statusCode === 200) {
+    try {
+      const body = JSON.parse(identityResponse.body);
+      const sessionUri = req.headers['mu-session-id'];
+      const { groupId, accountId, sessionId} = await linkSessionToUser(body, sessionUri);
+      res.status(201).send(
+        {
+          "data": {
+            "type": "remote-login",
+            "id": uuid(),
+            "relationships": {
+              "group": {
+                "links": {
+                  "related": `/bestuurseenheden/${groupId}`
+                },
+                "data": {
+                  "type": "bestuurseenheden",
+                  "id": groupId
+                }
+              },
+              "account": {
+                "links": {
+                  "related": `/accounts/${accountId}`
+                },
+                "data": {
+                  "type": "accounts",
+                  "id": accountId
+                }
+              },
+              "session": {
+                "links": {
+                  "related": "/sessions/current"
+                },
+                "id": sessionId
+              }
+            }
+          }
+        }
+      );
+    }
+    catch(e) {
+      console.log(e);
+      res.status(500).send({errors: ['error occured during processing:' + e]});
+    }
+  }
+  else {
+    console.log(identityResponse.statusCode);
+    res.status(502).send({errors: ['failed to fetch identity from endpoint']});
+  }
+} );
+
+app.use(errorHandler); 
